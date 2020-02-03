7098fc36ee8905bd4bc86f8a6834b941d77d0eb8
Kent Daleng <kent.stefan.daleng@nav.no>
2020-02-03 16:35:16 +0100

    Bruk useGet i aktivitetskrav

--- src/sider/Aktivitetskrav.js
+++ src/sider/Aktivitetskrav.js
@@ -4,30 +4,19 @@ import {Hovedknapp} from "nav-frontend-knapper";
 import {AlertStripeFeil} from "nav-frontend-alertstriper";
 import {Sider} from "../sider";
 import {API_URL} from "../App";
-import {useInput, useLocalStorageInput} from "../hooks";
+import {useGet, useInput, useLocalStorageInput} from "../hooks";
 
 export default function Aktivitetskrav() {
     const [fnr, fnrInput] = useLocalStorageInput({label: "Fødselsnummer", key: "fnr"});
     const [sykmeldingDok, sykmeldingDokInput] = useInput({label: "Sykmeldingsdokument"});
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [returverdi, setReturverdi] = useState("");
-    const [error, setError] = useState("");
+    const [get, isLoaded, returverdi, error] = useGet();
 
     const handleSubmit = (event) => {
         event.preventDefault();
         let url = new URL(API_URL + "/aktivitetskrav");
         let params = {brukerFnr: fnr, sykmeldingDok};
         url.search = new URLSearchParams(params).toString()
-        fetch(url.toString())
-            .then(res => res.text())
-            .then(res => {
-                setIsLoaded(true);
-                setReturverdi(res);
-            })
-            .catch(error => {
-                setIsLoaded(true);
-                setError(error.toString());
-            });
+        get(url.toString());
     };
 
     return (
